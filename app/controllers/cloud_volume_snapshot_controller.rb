11f7b00b5e833a858598f3413ce34d28f53c4499
swi2 <swi2@evo06.mendelu.cz>
2018-04-19 12:16:03 +0200

    Refactored method delete_cloud_volume_snapshots (I have the same refactoring as Jakub Kubista, cos we have the same controller and same issue)

--- app/controllers/cloud_volume_snapshot_controller.rb
+++ app/controllers/cloud_volume_snapshot_controller.rb
@@ -37,15 +37,14 @@ class CloudVolumeSnapshotController < ApplicationController
     assert_privileges("cloud_volume_snapshot_delete")
 
     binding.pry
-    snapshots = if @lastaction == "show_list" || (@lastaction == "show" && @layout != "cloud_volume_snapshot")
-		  find_checked_ids_with_rbac(CloudVolumeSnapshot)
-                else
-                  [find_id_with_rbac(CloudVolumeSnapshot, params[:id])]
-                end
-
-    if snapshots.empty?
-      add_flash(_("No Cloud Volume Snapshots were selected for deletion."), :error)
-    end
+    snapshots = find_checked_ids_with_rbac(CloudVolumeSnapshot, checked_or_params)
+ 
+	    #if @lastaction == "show_list" || (@lastaction == "show" && @layout != "cloud_volume_snapshot")
+	       #  find_checked_ids_with_rbac(CloudVolumeSnapshot)
+    	       #  find_records_with_rbac(CloudVolumeSnapshot)
+               # else
+               #  [find_id_with_rbac(CloudVolumeSnapshot, params[:id])]
+               # end
 
     snapshots_to_delete = []
     snapshots.each do |snapshot_id|
